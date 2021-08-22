d8eaf08c70fdbf8ecbc4e6b703868111613301b7
MELT <melt.ep00@gmail.com>
2021-08-23 00:31:49 +0900

    queryDSL 추가

---
+++ src/main/java/MELT/compensationTransactionAPI/config/QuerydslConfiguration.java
@@ -0,0 +1,26 @@
+package MELT.compensationTransactionAPI.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * Created by melodist
+ * User: MELT
+ * Date: 2021-08-22 022
+ * Time: 오후 11:58
+ */
+@Configuration
+public class QuerydslConfiguration {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
