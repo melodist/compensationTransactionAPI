256d8e2be262d868c0dfa19619a33b5d513552de
MELT <melt.ep00@gmail.com>
2021-09-18 22:44:56 +0900

    ENUM 추가

---
+++ src/main/java/MELT/compensationTransactionAPI/utils/orchestrator/enums/RestMethod.java
@@ -0,0 +1,11 @@
+package MELT.compensationTransactionAPI.utils.orchestrator.enums;
+
+/**
+ * Created by melodist
+ * User: MELT
+ * Date: 2021-09-18 018
+ * Time: 오후 8:05
+ */
+public enum RestMethod {
+    PUT, POST
+}
