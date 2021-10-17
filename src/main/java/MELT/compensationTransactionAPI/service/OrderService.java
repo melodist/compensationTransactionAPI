66e6852ea6593df0c8b888218dae6aebaadeeff4
MELT <melt.ep00@gmail.com>
2021-10-17 18:53:02 +0900

    주문 서비스 변경
    1. 상품 재고 차감, 증가 API 추가
    2. 보상 트랜잭션 API 호출 부분 변경

--- src/main/java/MELT/compensationTransactionAPI/service/OrderService.java
+++ src/main/java/MELT/compensationTransactionAPI/service/OrderService.java
@@ -1,12 +1,14 @@
 package MELT.compensationTransactionAPI.service;
 
 import MELT.compensationTransactionAPI.domains.Order;
+import MELT.compensationTransactionAPI.domains.item.ItemDto;
 import MELT.compensationTransactionAPI.repository.OrderRepository;
 import MELT.compensationTransactionAPI.utils.orchestrator.service.CompStdCallService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.HttpServerErrorException;
@@ -46,8 +48,8 @@ public class OrderService {
             log.debug("Billing Failed");
             // 보상 트랜잭션 API 호출
             Map<String, String> params = new HashMap<>();
-            params.put("id", "1");
-            params.put("count", String.valueOf(count));
+            params.put("id", String.valueOf(itemId));
+            params.put("stock", String.valueOf(count));
             compStdCallService.callCompensationTransaction("SVC01", params);
 
             // 주문 실패
@@ -57,12 +59,44 @@ public class OrderService {
         orderRepository.save(order);
     }
 
+    /**
+     * 상품의 재고를 차감한다.
+     * @param itemId : 상품 ID
+     * @param count : 차감할 재고량
+     */
     public void decreaseItem(Long itemId, int count) {
         RestTemplate restTemplate = new RestTemplate();
+
+        // 재고 확인
+        ResponseEntity<ItemDto> response = restTemplate.getForEntity("http://localhost:9100/item/{id}/", ItemDto.class, itemId);
+        ItemDto findItemDto = response.getBody();
+        log.debug("findItemDto : {}", findItemDto.toString());
+
+        // 재고 변경
         Map<String, String> params = new HashMap<>();
         params.put("id", itemId.toString());
-        params.put("count", String.valueOf(count));
-        restTemplate.put("http://localhost:9100/item/decreaseStock/{id}/{count}", null, params);
+        params.put("stock", String.valueOf(findItemDto.getStock() - count));
+        restTemplate.put("http://localhost:9100/item/{id}/{stock}", null, params);
+    }
+
+    /**
+     * 상품의 재고를 증가시킨다.
+     * @param itemId : 상품 ID
+     * @param count : 증가시킬 재고량
+     */
+    public void increaseItem(Long itemId, int count) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        // 재고 확인
+        ResponseEntity<ItemDto> response = restTemplate.getForEntity("http://localhost:9100/item/{id}/", ItemDto.class, itemId);
+        ItemDto findItemDto = response.getBody();
+        log.debug("findItemDto : {}", findItemDto.toString());
+
+        // 재고 변경
+        Map<String, String> params = new HashMap<>();
+        params.put("id", itemId.toString());
+        params.put("stock", String.valueOf(findItemDto.getStock() + count));
+        restTemplate.put("http://localhost:9100/item/{id}/{stock}", null, params);
     }
 
     public boolean createBill() {
