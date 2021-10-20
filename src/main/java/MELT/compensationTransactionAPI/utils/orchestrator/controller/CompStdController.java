a75bcf0e670f514666c1836d33382435278315ee
MELT <melt.ep00@gmail.com>
2021-10-20 22:26:30 +0900

    @ApiIgnore 추가

--- src/main/java/MELT/compensationTransactionAPI/utils/orchestrator/controller/CompStdController.java
+++ src/main/java/MELT/compensationTransactionAPI/utils/orchestrator/controller/CompStdController.java
@@ -13,12 +13,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/compStd")
 @RequiredArgsConstructor
+@ApiIgnore
 public class CompStdController {
 
     private final CompStdService compStdService;
