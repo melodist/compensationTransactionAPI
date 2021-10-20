5fdae2c0443146b6de4a9ca2f4218cf6883de6ed
MELT <melt.ep00@gmail.com>
2021-10-20 22:28:25 +0900

    주문 컨트롤러 변경
    1. URI 변경 (create 삭제)
    2. API 반환 양식 변경

--- src/main/java/MELT/compensationTransactionAPI/controller/OrderController.java
+++ src/main/java/MELT/compensationTransactionAPI/controller/OrderController.java
@@ -1,5 +1,7 @@
 package MELT.compensationTransactionAPI.controller;
 
+import MELT.compensationTransactionAPI.domains.Message;
+import MELT.compensationTransactionAPI.domains.OrderDto;
 import MELT.compensationTransactionAPI.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * Created by melodist
@@ -22,13 +25,23 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @PutMapping("/createOrder/{id}/{count}")
-    public ResponseEntity createOrder(@PathVariable Long id, @PathVariable int count) {
-        orderService.createOrder(id, count);
-        return new ResponseEntity(HttpStatus.OK);
+    @PutMapping("/order/{id}/{count}")
+    public ResponseEntity<Message<OrderDto>> createOrder(@PathVariable Long id, @PathVariable int count) {
+        Message<OrderDto> message;
+        OrderDto orderDto = new OrderDto(id, count);
+
+        if (orderService.createOrder(id, count)) {
+            message = new Message<>("200 OK", "주문에 성공하였습니다.", orderDto);
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        } else {
+            message = new Message<>("500 Internal Server Error"
+                    ,"주문이 실패하였습니다. 보상 트랜잭션 실행 내역을 확인해주세요", orderDto);
+            return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @PutMapping("/createCompTrx/{id}/{count}")
+    @PutMapping("/compTrx/{id}/{count}")
+    @ApiIgnore
     public ResponseEntity createCompTrx(@PathVariable Long id, @PathVariable int count) {
         orderService.increaseItem(id, count);
         return new ResponseEntity(HttpStatus.OK);
